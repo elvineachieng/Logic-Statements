dac7baa6877e2b110bd8e27c357f8df122356794
Elvine7067 <achiengelvine1@gmail.com>
2023-07-10 15:14:26 +0300

    switch case

---
+++ index1.js
@@ -0,0 +1,33 @@
+//switch statements
+/*syntax switch(expression) {
+ case value1:
+    // code to be executed
+    break;
+    case value2:
+    // code to be executed
+    break;
+    case value-n:
+    // code to be executed
+    break;
+   }
+   */
+   switch(activity) {
+    case "Get up":
+    console.log("It is 6:30AM");
+    break;
+    case "Breakfast":
+    console.log("It is 7:00AM");
+    break;
+    case "Drive to work":
+    console.log("It is 8:00AM");
+    break;
+    case "Lunch":
+    console.log("It is 12:00PM");
+    break; 
+    case "Drive home":
+    console.log("It is 5:00PM");
+    break; 
+    case "Dinner":
+    console.log("It is 6:30PM");
+    break;
+   }
