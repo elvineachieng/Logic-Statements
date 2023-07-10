026958400f81dd2984f76fce6ba976eb25e31fe8
Elvine7067 <achiengelvine1@gmail.com>
2023-07-10 12:25:32 +0300

    Logioc if and if else

---
+++ index.js
@@ -0,0 +1,9 @@
+//if and if else statements
+//if *some condition is true*, then *a certain action will happen*, else *another action will happen*
+let rain = true;
+if (rain){
+    console.log("Taking my umbrella when i need to go outside");
+}
+else {
+    console.log("I can leave my umbrella at home");
+}
