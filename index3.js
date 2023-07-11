9ccbb9635e0e801182a121010021528b00022d11
Elvine7067 <achiengelvine1@gmail.com>
2023-07-11 12:04:28 +0300

    1

---
+++ index3.js
@@ -0,0 +1,16 @@
+//Self check quiz
+const q = '1';
+switch (q){
+    case 1:
+        answer = "one";
+        break;
+    case 2:
+        answer = "this is the one";
+        break;
+    case 3:
+        answer = `1`;    
+    default:
+        answer = "not working";
+           break;
+        }
+console.log(answer);
