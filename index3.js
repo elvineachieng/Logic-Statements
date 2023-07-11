1a32b67038bf8369b6df3e2b140aa687f752887d
Elvine7067 <achiengelvine1@gmail.com>
2023-07-11 12:51:14 +0300

    5

--- index3.js
+++ index3.js
@@ -50,3 +50,11 @@ if (userNames.indexOf(userInput) > -1) {
     htmlOutput = "Denied, not a user ";
    }
    console.log(htmlOutput + ": " + userInput);
+   let a = 5;
+   let b = 10;
+   let c = 20;
+   let d = 30;
+   console.log(a > b || b > a);
+   console.log(a > b && b > a);
+   console.log(d > b || b > a);
+   console.log(d > b && b > a);
