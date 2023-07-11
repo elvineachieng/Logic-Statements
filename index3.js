7e5c118d48654476b37c443b352110fb446d0493
Elvine7067 <achiengelvine1@gmail.com>
2023-07-11 12:28:01 +0300

    3

--- index3.js
+++ index3.js
@@ -25,3 +25,20 @@ If login is false, the value assigned to userOkay will be the result of outputHo
 In both cases, the value assigned to outputHolder is also determined simultaneously. So, if login is true, outputHolder will be set to "logout", and if login is false, outputHolder will be set to "login" */
 let userOkay = login ? outputHolder = "logout" : outputHolder="login";
 console.log(userOkay);
+let myTime = 9;
+let output;
+if (myTime >= 8 && myTime < 12) {
+ output = "Wake up, it's morning";
+} else if (myTime >= 12 && myTime < 13) {
+ output = "Go to lunch";
+} else if (myTime >= 13 && myTime <= 16) {
+ output = "Go to work";
+} else if (myTime > 16 && myTime < 20) {
+ output = "Dinner time";
+} else if (myTime >= 22) {
+ output = "Time to go to sleep";
+} else {
+ output = "You should be sleeping";
+}
+console.log(output);//wake up its morning
+
