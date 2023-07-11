cd2f06fe2b859a716bb9a9bf167fd5fbaf899cf2
Elvine7067 <achiengelvine1@gmail.com>
2023-07-11 13:04:20 +0300

    logic statements

--- index3.js
+++ index3.js
@@ -62,3 +62,10 @@ if (userNames.indexOf(userInput) > -1) {
    console.log(d > b || b > a);
    //true
    console.log(d > b && b > a);
+let val = 100;
+let message = (val > 100) ? `${val} was greater than 100` : 
+`${val} was LESS or Equal to 100`;
+console.log(message);
+let check = (val % 2) ? `Odd` : `Even`;
+check = `${val} is ${check}`;
+console.log(check);
